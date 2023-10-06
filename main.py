5521f33e722f1fa894ee3cf4ff1e98aec6e67b17
Daniel Medina <72998479+medinardaniel@users.noreply.github.com>
2023-10-06 08:10:21 +0000

    first commit

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ ETL-Query script
 """
 
 from mylib.extract import extract
-from mylib.transform_load import load
-from mylib.query import create, read, update, delete
-import fire
+from mylib.load_transform import load
+from mylib.complex_query import complex_query
 
-def main(command=None):
+def main():
 
     # Extract
     print("Extracting data...")
@@ -17,31 +16,10 @@ def main(command=None):
     print("Transforming data...")
     load()
 
-    # Query
-    if command == "r":
-        print("Read top 5 rows...")
-        read()
-        return
-    elif command == 'c':
-        print("Create a new record for North Carolina...")
-        print(create())
-    elif command == 'u':
-        print("Update the record for North Carolina...")
-        print(update())
-    elif command == 'd':
-        print("Delete record for North Carolina...")
-        print(delete())
-    elif command == 'all' or command == None:
-        print("Read top 5 rows...")
-        read()
-        print("Create a new record for North Carolina...")
-        print(create())
-        print("Update the record for North Carolina...")
-        print(update())
-        print("Delete record for North Carolina...")
-        print(delete())
+    # complex query
+    print("Running complex query...")
+    complex_query()
 
 
 if __name__ == "__main__":
     main()
-    fire.Fire(main)
--- mylib/extract.py
+++ mylib/extract.py
@@ -7,18 +7,33 @@ import requests
 import pandas as pd
 import io
 
-def extract(url="https://raw.githubusercontent.com/emorisse/FBI-Hate-Crime-Statistics/master/2013/table13.csv", 
-            file_path="/workspaces/sqlite-lab-dm/data/HateCrimes.csv"):
+def extract(url1="https://raw.githubusercontent.com/datasets/football-datasets/master/datasets/la-liga/season-1819.csv", 
+            url2="https://raw.githubusercontent.com/datasets/football-datasets/master/datasets/premier-league/season-1819.csv",
+            file_path1="/workspaces/ids706-miniproj-6/data/laliga.csv",
+            file_path2="/workspaces/ids706-miniproj-6/data/epl.csv"):
     """"Extract a url to a file path"""
-    with requests.get(url) as r:
+    with requests.get(url1) as r:
         df = pd.read_csv(io.StringIO(r.text))
-        df = df.rename(columns={"State":"state", "Agency type":"agency_type", "Agency name":"agency_name", "Race":"race",
-                                "Religion":"religion", "Sexual orientation":"sexual_orientation","Ethnicity":"ethnicity",
-                                "Disability":"disability", "Gender":"gender", "Gender Identity":"gender_identity",
-                                "1st quarter":"q1","2nd quarter":"q2","3rd quarter":"q3","4th quarter":"q4","Population":"population"
-                                })
-        df.to_csv(file_path, index=False)
-    return file_path
-
-
-
+        # rename columns
+        df = df.rename(columns={"FTHG": "home_goals", "FTAG": "away_goals", "FTR": "result",
+                                "HTHG": "home_goals_half_time", "HTAG": "away_goals_half_time",
+                                "HTR": "result_half_time", "HS": "home_shots", "AS": "away_shots",
+                                "HST": "home_shots_on_target", "AST": "away_shots_on_target", "HF": "home_fouls",
+                                "AF": "away_fouls", "HC": "home_corners", "AC": "away_corners",
+                                "HY": "home_yellow_cards", "AY": "away_yellow_cards", "HR": "home_red_cards",
+                                "AR": "away_red_cards"})
+        df.to_csv(file_path1, index=False)
+    with requests.get(url2) as r:
+        df = pd.read_csv(io.StringIO(r.text))
+        # drop referee column
+        df = df.drop(columns=["Referee"])
+        # rename columns
+        df = df.rename(columns={"FTHG": "home_goals", "FTAG": "away_goals", "FTR": "result",
+                        "HTHG": "home_goals_half_time", "HTAG": "away_goals_half_time",
+                        "HTR": "result_half_time", "HS": "home_shots", "AS": "away_shots",
+                        "HST": "home_shots_on_target", "AST": "away_shots_on_target", "HF": "home_fouls",
+                        "AF": "away_fouls", "HC": "home_corners", "AC": "away_corners",
+                        "HY": "home_yellow_cards", "AY": "away_yellow_cards", "HR": "home_red_cards",
+                        "AR": "away_red_cards"})
+        df.to_csv(file_path2, index=False)
+    return file_path1, file_path2
